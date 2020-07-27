1ee229caee7db0a7517cf791c9ebff241fd8f78f
Graeme Norris <gnorris@shawnigan.ca>
2020-07-27 15:34:10 -0700

    added search system for flights using advanced forms

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,17 @@
 class FlightsController < ApplicationController
     def index
         @flights = Flight.all
+        @flight_options = Airport.all.map { |a| [ a.code, a.id ] }
+        @flight_date = Flight.all.map { |f| [f.start_date, f.start_date] }
     end 
 
     def search
-        @flight = Flight.where()
+        @flights = Flight.where("from_airport_id LIKE ? AND to_airport_id LIKE ? OR start_date LIKE ?", 
+                                params[:from_airport], params[:to_airport], params[:start_date]) 
+                                
+        flash[:no_data] = "no flights found"
     end 
+    #:from_airport_id => :from_airport, 
+    #:to_airport_id => :to_airport, 
+    #:start_date => :start_date).all
 end
